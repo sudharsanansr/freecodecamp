a2dc9e45ab7255f4dceeef0470cef9665f5ce368
Sudharsanan Ravichandran <sudharsanv36@gmail.com>
2020-05-28 17:35:45 +0530

    indent added

--- switchCase.js
+++ switchCase.js
@@ -5,21 +5,21 @@ function caseInSwitch(val) {
         case 1:
         case 2:
         case 3:
-        answer = 'Low';
-        break;
+          answer = 'Low';
+          break;
         case 4:
         case 5:
         case 6:
-        answer = 'Medium';
-        break;
+          answer = 'Medium';
+          break;
         case 7:
         case 8:
         case 9:
-        answer = 'High';
-        break;
+          answer = 'High';
+          break;
     }
     // Only change code above this line
     return answer;
   }
   
-console.log(caseInSwitch(1));
+console.log(caseInSwitch(4));
