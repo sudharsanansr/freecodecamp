ff1be5509d14b39a20a6bcbedf66bbaca8675ddf
Sudharsanan Ravichandran <sudharsanv36@gmail.com>
2020-05-29 13:04:03 +0530

    commit

---
+++ profileLookUp.js
@@ -0,0 +1,52 @@
+// Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop){
+// Only change code below this line
+    for(var i = 0; i < contacts.length; i++){
+        if(contacts[i]['firstName'] == name){
+            if(contacts[i].hasOwnProperty(prop)){
+                return contacts[i][prop];
+            }
+            else{
+                return "No such property";
+            }
+        }
+    }
+    return "No such contact";
+}
+
+// Only change code above this line
+
+console.log(lookUpProfile("Akira", "likes"));
+console.log(lookUpProfile("Kristian", "lastName"));
+console.log(lookUpProfile("Harry", "likes"));
+console.log(lookUpProfile("Bob", "number"));
+console.log(lookUpProfile("Bob", "potato"));
+console.log(lookUpProfile("Akira", "address"));
